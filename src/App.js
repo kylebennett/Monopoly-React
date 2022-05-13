6661e245358ed519b5f311ebc7d8c6545e6ac416
Kyle Bennett <kyle.bennett@sage.com>
2022-05-13 01:00:09 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    players: [],
+  };
+
+  async componentDidMount() {
+    const response = await fetch("/players");
+    const body = await response.json();
+    this.setState({ players: body });
+  }
+
+  render() {
+    const { players } = this.state;
+    return (
+      <div className="App">
+        <h2>Players</h2>
+        {players.map((player) => (
+          <div key={player.id}>{player.name}</div>
+        ))}
+      </div>
+    );
+  }
 }
-
 export default App;
